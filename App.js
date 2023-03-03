41b0ccada6cdda0d7d2461a07ea9007e25f24043
Muhammad Arslan <assoarslan@gmail.com>
2023-03-03 23:40:41 +0500

    portfolio
    portfolio

---
+++ App.js
@@ -0,0 +1,13 @@
+// var input = prompt("enter number...");
+// // console.log(typeof(input));
+// var neo = [];
+// neo = input.split("");
+// // console.log(neo);
+// var result = 0;
+// for (var i = 0; i < neo.length; i++) {
+//    result = result + +neo[i];
+// }
+// console.log(result);
+
+
+// var input = prompt("enter number");
